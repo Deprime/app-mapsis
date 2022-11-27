a1463502030ce6c8a9c62b3f350354da1d04e6cd
Andrey Dyugaev <deprimehell@gmail.com>
2022-11-27 21:43:59 +0700

    Update vite.config.ts
    Build output directory changed aacording to hosting base directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+  build: {
+    outDir: "public_html",
+  }
 };
 
 export default config;
